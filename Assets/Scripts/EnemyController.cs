2fef7cd49c716bdbae210a1905fc9bdcbea8ae62
Saikou17 <a01783208@itesm.mx>
2024-09-19 19:29:43 -0600

    Final Product

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,16 +5,14 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-
-    //Atributos
-
-    //Patrullaje
+    // Atributos
+    // Patrullaje
     [SerializeField] List<Transform> puntosPatrullaje;
     private int currentPatrolPoint;
+    Transform puntoFinal;
 
-    //Enemigo
+    // Enemigo
     public NavMeshAgent agent;
-
 
     // Start is called before the first frame update
     void Start()
@@ -22,17 +20,12 @@ public class EnemyController : MonoBehaviour
         Patrullar(puntosPatrullaje);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    //Función para patrullar
+    // Función para patrullar
     public void Patrullar(List<Transform> puntos)
     {
-        //Verificamos que la lista no este vacia o que tenga un solo elemento
-        if(puntos.Count <= 1) return;
+        // Verificamos que la lista no esté vacía o que tenga un solo elemento
+        if (puntos.Count <= 1) return;
+        Debug.Log("Entro");
         int newPatrolPoint;
         do
         {
@@ -40,7 +33,6 @@ public class EnemyController : MonoBehaviour
         } while (newPatrolPoint == currentPatrolPoint);
         currentPatrolPoint = newPatrolPoint;
         agent.SetDestination(puntos[currentPatrolPoint].position);
-            
     }
 
     public void Perseguir(Transform player)
@@ -48,24 +40,67 @@ public class EnemyController : MonoBehaviour
         agent.SetDestination(player.position);
     }
 
+    // Modificamos el método para aceptar un GameObject
+    public void ReiniciarBusqueda(GameObject other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Dame la distancia al punto más cercano
+            float distance = float.MaxValue;
+            int index = 0;
+            foreach (var punto in puntosPatrullaje)
+            {
+                index ++;
+                float newDistance = Vector3.Distance(punto.position, transform.position);
+                if (newDistance < distance)
+                {
+                    distance = newDistance;
+                    puntoFinal = punto;
+                }
+            }
+            currentPatrolPoint = index;
+            agent.SetDestination(puntoFinal.position);
+        }
+    }
+
+    // Triggers
     public void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Punto"){
+        if (other.CompareTag("Punto"))
+        {
             Patrullar(puntosPatrullaje);
         }
     }
 
     public void OnTriggerStay(Collider other)
     {
-        if(other.tag == "Player"){
+        if (other.CompareTag("Player"))
+        {
             Perseguir(other.transform);
         }
     }
 
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ReiniciarBusqueda(other.gameObject); // Pasar el GameObject del jugador
+        }
+    }
+
+    // Collisions
     public void OnCollisionEnter(Collision other)
     {
-        if(other.gameObject.tag == "Player"){
-            Debug.Log("Attack -10 life");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerController playerController = other.gameObject.GetComponent<PlayerController>();
+            playerController.vida -= 1;
+
+            if (playerController.vida == 0)
+            {
+                Debug.Log("Llamar");
+                ReiniciarBusqueda(other.gameObject); // Pasar el GameObject del jugador
+            }
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,30 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.TextCore.Text;
 
 public class PlayerController : MonoBehaviour
 {
     public CharacterController _character { get; set; }
     public Transform respawn;
 
-    //Estad�sticas
-    private int vida = 3;
+    // Estadísticas
+    public int vida {get; set;}
     private float velocidad = 7f;
     private Vector3 movimiento;
+
+    // Variables para la gravedad
+    private float velocidadVertical = 0f;
+    private const float gravedad = -9.81f;
 
     // Start is called before the first frame update
     void Start()
     {
+        vida = 3;
         _character = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (vida == 0)
         {
-            //Game Over
+            // Game Over
             vida = 3;
             Respawn(respawn);
         }
@@ -37,17 +42,36 @@ public class PlayerController : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal"); 
         float moveZ = Input.GetAxis("Vertical"); 
 
+        // Movimiento en el plano horizontal
         movimiento = new Vector3(moveX, 0, moveZ);
 
         if (movimiento.magnitude > 1)
         {
             movimiento.Normalize();
         }
+
+        // Comprobar si estamos en el suelo
+        if (_character.isGrounded)
+        {
+            velocidadVertical = 0f; // Reiniciar la velocidad vertical al tocar el suelo
+        }
+        else
+        {
+            // Aplicar gravedad si no estamos en el suelo
+            velocidadVertical += gravedad * Time.deltaTime;
+        }
+
+        // Añadir la componente vertical (gravedad)
+        movimiento.y = velocidadVertical;
+
+        // Mover al personaje
         _character.Move(movimiento * velocidad * Time.deltaTime);
     }
 
     private void Respawn(Transform respawnPoint)
     {
+        gameObject.SetActive(false);
         transform.position = respawnPoint.position;
+        gameObject.SetActive(true);
     }
 }
