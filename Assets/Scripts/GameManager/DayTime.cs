84cc86229177ec9a81b9b814303be50bc020cdd3
Saikou17 <a01783208@itesm.mx>
2024-10-03 21:11:34 -0600

    Bayesian Nodes

--- Assets/Scripts/GameManager/DayTime.cs
+++ Assets/Scripts/GameManager/DayTime.cs
@@ -7,8 +7,14 @@ public class DayTime : MonoBehaviour
     //Obtensmoa la luz de la escena
     public Light sceneLight;
 
+    //Contador de la luna de sangre
+    public float moonSangreDuration = 300f;
+
     //Obtenemos la duracion del dia en segundos
     public float dayDuration = 300f;
+
+    //Script de red bayesiana
+    [SerializeField] BayesianManagerWeather weatherManager;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +25,23 @@ public class DayTime : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        LightControl();
+        if(sceneLight.color != Color.red)
+        {
+            LightControl();
+        }
+        else if(sceneLight.color == Color.red)
+        {
+            //Actualizamos el contador
+            moonSangreDuration -= Time.deltaTime;
+        }
+        if(moonSangreDuration <= 0f)
+        {
+            //Cambio de estado
+            sceneLight.color = Color.white;
+            //Reseteamos el contador
+            moonSangreDuration = 300f;
+        }
+        
     }
 
     public void LightControl()
@@ -28,6 +50,8 @@ public class DayTime : MonoBehaviour
 
         //Esta funcion es la funcion de las ondas sin que va desde 0 a 1 siendo 1 como el punto maximo del dia
         float lighIntensity = 0.5f + Mathf.Sin(Time.time * 2.0f * Mathf.PI/dayDuration) / 2.0f;
+        //Llamada a los estatico
+        weatherManager._sceneLight = lighIntensity;
         //Modificamos la luz de acuerdo a la intensidad
         GetComponent<Light>().intensity = lighIntensity; 
     }
